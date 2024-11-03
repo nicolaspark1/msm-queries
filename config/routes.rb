ab6d46107b0500b7c1b303526390b770c572665f
nicolaspark1 <141599069+nicolaspark1@users.noreply.github.com>
2024-11-03 22:19:24 +0000

    movie start

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
-  get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/", { :controller => "directors", :action => "index" })
+
+  get("/directors/:the_id", { :controller => "directors", :action => "show" })
+
   
+
 end
